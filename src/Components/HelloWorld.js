f4e8128d1e343d6d683ea357727fd85381081f92
Parisa Foroudian <parisafro.pf@gmail.com>
2020-04-30 12:16:37 +0430

    add some style

--- src/Components/HelloWorld.js
+++ src/Components/HelloWorld.js
@@ -4,9 +4,9 @@ import flower from './flower.jpeg'
 export class HelloWorld extends Component{
     render(){
         return(
-                <div>
-                    <img src={flower}/>
-                </div>
+                
+                <img style={{height:500,width:400,paddingTop:150}} src={flower}/>
+            
         )
     }
 }
